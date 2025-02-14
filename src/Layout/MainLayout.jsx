c7cf4e2f6ef5e0c7e44b3a33f01e4775e025ff34
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-02-15 01:33:12 +0600

    Loader added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -3,9 +3,22 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "../Pages/Home/Shared/Footer/Footer";
 import MyNavbar from "../Pages/Home/Shared/Navbar/MyNavbar";
+import { useContext } from "react";
+import Authcontext from "../context/Authcontext/Authcontext";
+import LoadingHeart from "../LoadingHeart/LoadingHeart";
+
+
+
 
 
 const MainLayout = () => {
+    const {loading}=useContext(Authcontext)
+
+if(loading){
+    return <LoadingHeart/>;
+}
+
+
     return (
         <div className="items-center justify-center mx-auto max-w-7xl">
             <MyNavbar />
@@ -13,6 +26,8 @@ const MainLayout = () => {
             <Footer />
         </div>
     );
-};
+
+}
+   
 
 export default MainLayout;
